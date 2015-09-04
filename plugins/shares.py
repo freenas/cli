3d9df30a7d5b1dc8ae6b3b3ba8da51f8b9c85629
Erin Clark <erin@ixsystems.com>
2015-09-04 14:48:23 -0700

    Since all the others either say _ namespace in the description, so should shares for a little consistency

--- plugins/shares.py
+++ plugins/shares.py
@@ -44,7 +44,7 @@ class ConnectedUsersCommand(Command):
         output_list(result, _("IP address"))
 
 
-@description("Shares")
+@description("Shares namespace")
 class SharesNamespace(Namespace):
     def __init__(self, name, context):
         super(SharesNamespace, self).__init__(name)

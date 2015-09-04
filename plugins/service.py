63916e6cc79e36fb24712bb1ecba56d0b15e994d
Erin Clark <erin@ixsystems.com>
2015-09-04 14:42:29 -0700

    Allow configuring services through the cli for the ones currently supported by the middleware
    Ticket: #11336

--- plugins/service.py
+++ plugins/service.py
@@ -53,7 +53,7 @@ class ServiceManageCommand(Command):
         context.submit_task('service.manage', self.parent.primary_key, self.action)
 
 
-@description("Service namespace")
+@description("Services namespace")
 class ServicesNamespace(RpcBasedLoadMixin, EntityNamespace):
     def __init__(self, name, context):
         super(ServicesNamespace, self).__init__(name, context)
@@ -103,6 +103,8 @@ class ServiceConfigNamespace(ConfigNamespace):
             type=ValueType.BOOLEAN
         )
 
+        self.get_properties(parent.name)
+
     def load(self):
         self.entity = self.context.call_sync('services.get_service_config', self.parent.entity['name'])
         self.orig_entity = copy.deepcopy(self.entity)
@@ -116,6 +118,165 @@ class ServiceConfigNamespace(ConfigNamespace):
             self.parent.entity['name'],
             self.get_diff(),
             callback=lambda s: post_save())
+
+    def get_properties(self, name):
+        if name == "sshd":
+            self.add_property(
+                descr='sftp log facility',
+                name='sftp_log_facility',
+                get='sftp_log_facility',
+                type=ValueType.STRING
+            )
+            self.add_property(
+                descr='Allow public key authentication',
+                name='allow_pubkey_auth',
+                get='allow_pubkey_auth',
+                type=ValueType.BOOLEAN
+            )
+            self.add_property(
+                descr='Enable compression',
+                name='compression',
+                get='compression',
+                type=ValueType.BOOLEAN
+            )
+            self.add_property(
+                descr='Allow password authentication',
+                name='allow_password_auth',
+                get='allow_password_auth',
+                type=ValueType.BOOLEAN
+            )
+            self.add_property(
+                descr='Allow port forwarding',
+                name='allow_port_forwarding',
+                get='allow_port_forwarding',
+                type=ValueType.BOOLEAN
+            )
+            self.add_property(
+                descr='Permit root login',
+                name='permit_root_login',
+                get='permit_root_login',
+                type=ValueType.BOOLEAN
+            )
+            self.add_property(
+                descr='sftp log level',
+                name='sftp_log_level',
+                get='sftp_log_level',
+                type=ValueType.STRING
+            )
+            self.add_property(
+                descr='Port',
+                name='port',
+                get='port',
+                type=ValueType.NUMBER
+            )
+        elif name == "nginx":
+            self.add_property(
+                descr='Redirect http to https',
+                name='http.redirect_https',
+                get='http.redirect_https',
+                type=ValueType.BOOLEAN
+            )
+            self.add_property(
+                descr='Enable http',
+                name='http.enable',
+                get='http.enable',
+                type=ValueType.BOOLEAN
+            )
+            self.add_property(
+                descr='http port',
+                name='http.port',
+                get='http.port',
+                type=ValueType.NUMBER
+            )
+            self.add_property(
+                descr='Enable https',
+                name='https.enable',
+                get='https.enable',
+                type=ValueType.BOOLEAN
+            )
+            self.add_property(
+                descr='https port',
+                name='https.port',
+                get='https.port',
+                type=ValueType.NUMBER
+            )
+            self.add_property(
+                descr='Https certificate',
+                name='https.certificate',
+                get='https.certificate',
+                type=ValueType.STRING
+            )
+            self.add_property(
+                descr='Listen on IP',
+                name='listen',
+                get='listen',
+                list=True,
+                type=ValueType.SET
+            )
+        elif name == "ftp":
+            self.add_property(
+                descr='ftp port',
+                name='port',
+                get='port',
+                type=ValueType.NUMBER
+            )
+        elif name == "afp":
+            self.add_property(
+                descr='Share Home Directory',
+                name='homedir_enable',
+                get='homedir_enable',
+                type=ValueType.BOOLEAN
+                )
+            self.add_property(
+                descr='Home Directory Path',
+                name='homedir_path',
+                get='homedir_path',
+                type=ValueType.STRING
+                )
+            self.add_property(
+                descr='Home Directory Name',
+                name='homedir_name',
+                get='homedir_name',
+                type=ValueType.STRING
+                )
+            self.add_property(
+                descr='Auxiliary Parameters',
+                name='auxiliary',
+                get='auxiliary',
+                type=ValueType.STRING
+                )
+            self.add_property(
+                descr='Connections limit',
+                name='connections_limit',
+                get='connections_limit',
+                type=ValueType.NUMBER
+                )
+            self.add_property(
+                descr='Guest user',
+                name='guest_user',
+                get='guest_user',
+                type=ValueType.STRING
+                )
+            self.add_property(
+                descr='Enable guest user',
+                name='guest_enable',
+                get='guest_enable',
+                type=ValueType.BOOLEAN
+                )
+            self.add_property(
+                descr='Bind Addresses',
+                name='bind_addresses',
+                get='bind_addresses',
+                list=True,
+                type=ValueType.SET
+                )
+            self.add_property(
+                descr='Database Path',
+                name='dbpath',
+                get='dbpath',
+                type=ValueType.STRING
+                )
+
 
 def _init(context):
     context.attach_namespace('/', ServicesNamespace('services', context))

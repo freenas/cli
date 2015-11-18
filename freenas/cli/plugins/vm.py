4a4e33b1f84532b23316f58a4997c401cb6a8e62
Jakub Klama <jakub@ixsystems.com>
2015-11-18 20:54:16 +0100

    Add initial CLI plugin for managing bhyve VMs.

---
+++ freenas/cli/plugins/vm.py
@@ -0,0 +1,205 @@
+#
+# Copyright 2015 iXsystems, Inc.
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+#####################################################################
+
+import gettext
+from freenas.cli.namespace import (
+    Namespace, EntityNamespace, Command, IndexCommand,
+    RpcBasedLoadMixin, TaskBasedSaveMixin, description,
+    CommandException, ListCommand
+)
+from freenas.cli.output import ValueType, Table
+from freenas.utils import first_or_default
+
+
+t = gettext.translation('freenas-cli', fallback=True)
+_ = t.gettext
+
+
+class StartVMCommand(Command):
+    def __init__(self, parent):
+        self.parent = parent
+
+    def run(self, context, args, kwargs, opargs):
+        context.call_task_sync('container.start', self.parent.entity['id'])
+
+
+class StopVMCommand(Command):
+    def __init__(self, parent):
+        self.parent = parent
+
+    def run(self, context, args, kwargs, opargs):
+        context.call_task_sync('container.stop', self.parent.entity['id'])
+
+
+class RebootVMCommand(Command):
+    def __init__(self, parent):
+        self.parent = parent
+
+    def run(self, context, args, kwargs, opargs):
+        pass
+
+
+@description("Configure and manage virtual machines")
+class VMNamespace(TaskBasedSaveMixin, RpcBasedLoadMixin, EntityNamespace):
+    def __init__(self, name, context):
+        super(VMNamespace, self).__init__(name, context)
+        self.query_call = 'container.query'
+        self.create_task = 'container.create'
+        self.update_task = 'container.update'
+        self.delete_task = 'container.delete'
+        self.primary_key_name = 'name'
+
+        self.skeleton_entity = {
+            'type': 'VM',
+            'devices': [],
+            'config': {}
+        }
+
+        self.add_property(
+            descr='VM Name',
+            name='name',
+            get='name',
+            list=True
+        )
+
+        self.add_property(
+            descr='State',
+            name='state',
+            get='status.state',
+            set=None
+        )
+
+        self.add_property(
+            descr='Volume',
+            name='volume',
+            get='target',
+            createsetable=True,
+            usersetable=False
+        )
+
+        self.add_property(
+            descr='Description',
+            name='description',
+            get='description',
+            list=False
+        )
+
+        self.add_property(
+            descr='Memory size (MB)',
+            name='memsize',
+            get='config.memsize',
+            list=True,
+            type=ValueType.NUMBER
+        )
+
+        self.add_property(
+            descr='CPU cores',
+            name='cores',
+            get='config.ncpus',
+            list=True,
+            type=ValueType.NUMBER
+        )
+
+        self.add_property(
+            descr='CD image',
+            name='cdimage',
+            get='config.cdimage',
+            list=False
+        )
+
+        self.add_property(
+            descr='Bootloader type',
+            name='bootloader',
+            get='config.bootloader',
+            list=False,
+            enum=['BHYVELOAD', 'GRUB']
+        )
+
+        self.primary_key = self.get_mapping('name')
+        self.entity_namespaces = lambda this: [
+            VMDisksNamespace('disks', self.context, this)
+        ]
+
+        self.entity_commands = lambda this: {
+            'start': StartVMCommand(this),
+            'stop': StopVMCommand(this),
+            'reboot': RebootVMCommand(this)
+        }
+
+
+class VMDisksNamespace(EntityNamespace):
+    def __init__(self, name, context, parent):
+        super(VMDisksNamespace, self).__init__(name, context)
+        self.parent = parent
+        self.skeleton_entity = {
+            'type': 'DISK',
+            'properties': {}
+        }
+
+        self.add_property(
+            descr='Disk ID',
+            name='id',
+            get='id'
+        )
+
+        self.add_property(
+            descr='Disk name',
+            name='name',
+            get='name'
+        )
+
+        self.add_property(
+            descr='Disk size',
+            name='size',
+            get='properties.size',
+            type=ValueType.SIZE
+        )
+
+    def get_one(self, name):
+        return first_or_default(lambda a: a['name'] == name, self.parent.entity.get('devices', []))
+
+    def query(self, params, options):
+        return self.parent.entity['devices'] or []
+
+    def save(self, this, new=False):
+        if new:
+            if self.parent.entity['devices'] is None:
+                 self.parent.entity['devices'] = []
+            self.parent.entity['devices'].append(this.entity)
+        else:
+            entity = first_or_default(lambda a: a['name'] == this.entity['name'], self.parent.entity['devices'])
+            entity.update(this.entity)
+
+        self.parent.save()
+
+    def delete(self, name):
+        self.parent.entity['devices'] = [a for a in self.parent.entity['devices'] if a['name'] == name]
+        self.parent.save()
+
+
+def _init(context):
+    context.attach_namespace('/', VMNamespace('vm', context))

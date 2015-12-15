ff5bcc0e43f1e2d30c20d069d45dd333251c806e
Erin Clark <erin@ixsystems.com>
2015-12-15 14:01:34 -0800

    Make urlparse work in python2
    Ticket: #10615

--- freenas/cli/repl.py
+++ freenas/cli/repl.py
@@ -44,7 +44,6 @@ import getpass
 import traceback
 import six
 import paramiko
-from urllib.parse import urlparse
 from socket import gaierror as socket_error
 from freenas.cli.descriptions import events
 from freenas.cli import functions
@@ -78,6 +77,11 @@ try:
     from shutil import get_terminal_size
 except ImportError:
     from backports.shutil_get_terminal_size import get_terminal_size
+
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 if platform.system() == 'Darwin':
     import gnureadline as readline

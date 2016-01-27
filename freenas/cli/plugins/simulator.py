c2567a0fd9f50d59464d4394c9ede65e3ce84201
Erin Clark <erin@ixsystems.com>
2016-01-27 15:56:47 -0800

    Tell user if simulator service is not running when they create a disk
    Ticket: #13353

--- freenas/cli/plugins/simulator.py
+++ freenas/cli/plugins/simulator.py
@@ -25,8 +25,14 @@
 #
 #####################################################################
 
+import gettext
 from freenas.cli.namespace import Namespace, EntityNamespace, Command, RpcBasedLoadMixin, TaskBasedSaveMixin, description
-from freenas.cli.output import ValueType
+from freenas.cli.output import ValueType, output_msg_locked
+from freenas.cli.utils import post_save
+
+
+t = gettext.translation('freenas-cli', fallback=True)
+_ = t.gettext
 
 
 @description("Tools for simulating disks")
@@ -99,6 +105,32 @@ class DisksNamespace(RpcBasedLoadMixin, TaskBasedSaveMixin, EntityNamespace):
         )
 
         self.primary_key = self.get_mapping('name')
+
+    def save(self, this, new=False):
+        if new:
+            self.context.submit_task(
+                self.create_task,
+                this.entity,
+                callback=lambda s: self.post_save(this, s, new))
+            return
+
+        self.context.submit_task(
+            self.update_task,
+            this.orig_entity[self.save_key_name],
+            this.get_diff(),
+            callback=lambda s: self.post_save(this, s, new))
+
+    def post_save(self, this, status, new):
+        service_name = 'simulator'
+        if status == 'FINISHED':
+            service = self.context.call_sync('service.query', [('name', '=', service_name)], {'single': True})
+            if service['state'] != 'RUNNING':
+                if new:
+                    action = "created"
+                else:
+                    action = "updated"
+                output_msg_locked(_("Disk '{0}' has been {1} but the service '{2}' is not currently running, please enable the service with '/ service {2} config set enable=yes'".format(this.entity['id'], action, service_name)))
+        post_save(this, status)
 
 
 @description("Tools for simulating aspects of a NAS")

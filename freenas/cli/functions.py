1c26a0a5f6a5d0b970d83f779848ba15a3809c94
Suraj Ravichandran <suraj@ixsystems.com>
2016-02-05 15:33:53 -0800

    Add `factorial` function to the cli.
    Ticket: #13372

--- freenas/cli/functions.py
+++ freenas/cli/functions.py
@@ -146,6 +146,14 @@ def table(data, columns):
     return Table(data, [Table.Column(l, a) for l, a in columns])
 
 
+# Add functions to help script things
+
+def factorial(n):
+    "Computes the factorial of positive integers and zero"
+    if n <= 1:
+        return 1
+    return n*factorial(n-1)
+
 functions = {
     'print': print_,
     'printf': printf,
@@ -172,5 +180,6 @@ functions = {
     'freadline': freadline,
     'fprintf': fprintf,
     'fclose': fclose,
-    'table': table
+    'table': table,
+    'factorial': factorial
 }

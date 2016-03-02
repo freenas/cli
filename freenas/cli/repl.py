da3707205e77e9ee1830f84c33779a39cffc05d0
Dru Lavigne <dru@freebsd.org>
2016-03-02 11:06:32 -0500

    Tweak help text.

--- freenas/cli/repl.py
+++ freenas/cli/repl.py
@@ -245,7 +245,7 @@ class VariableStore(object):
             'tasks_blocking': _('Toggle tasks blocking console output. Can be set to yes or no.'),
             'show_events': _('Toggle displaying of events. Can be set to yes or no.'),
             'debug': _('Toggle display of debug messages. Can be set to yes or no.'),
-            'abort_on_errors': _('Toggle console aborting on errors. Can be set to yes or no.'),
+            'abort_on_errors': _('Can be set to yes or no. When set to yes, command execution will abort on syntax or command errors.'),
             'output': _('Either send all output to specified file or set to \'none\' to display output on the console.'),
             'verbosity': _('Increasing verbosity of event messages. Can be set from 1 to 5')
         }

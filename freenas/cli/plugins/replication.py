f1dddb4dc0e0c453e6676e1588a901a4eeeb1e66
Dru Lavigne <dru@freebsd.org>
2016-04-07 13:40:23 -0400

    Match case and punctuation

--- freenas/cli/plugins/replication.py
+++ freenas/cli/plugins/replication.py
@@ -216,7 +216,7 @@ class CreateReplicationCommand(Command):
         ]
 
 
-@description(_("Provides commands for listing and managing replication tasks."))
+@description(_("List and manage replication tasks"))
 class ReplicationTaskNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, EntityNamespace):
     """
     The replication namespace provides commands for listing and managing replication tasks.
@@ -385,7 +385,7 @@ class CreateHostsPairCommand(Command):
         ]
 
 
-@description(_("Manage replication tasks and known replication hosts"))
+@description(_("Manage replication hosts"))
 class ReplicationHostNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, EntityNamespace):
     """
     The replication namespace provides commands for listing and managing replication tasks.

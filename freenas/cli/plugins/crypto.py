8d5e5aa19e04f0ad844e02ed533fbcd744dccfb5
Piotr Glinianowicz <piotr@ixsystems.com>
2016-05-30 21:28:02 +0200

    ADDED: Import CA/Cert command definition and some other fixes to the 'crypto' namespace. Attached 'crypto' namespace to root namespace. Ticket: #15095

--- freenas/cli/plugins/crypto.py
+++ freenas/cli/plugins/crypto.py
@@ -33,6 +33,7 @@ from freenas.cli.namespace import (
     EntitySubscriberBasedLoadMixin, description, CommandException
 )
 from freenas.cli.output import ValueType
+from freenas.cli.complete import EnumComplete, NullComplete
 
 
 t = gettext.translation('freenas-cli', fallback=True)
@@ -74,7 +75,7 @@ class CertificateAuthorityNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoa
             ('type', 'in', ('CA_EXISTING', 'CA_INTERMEDIATE', 'CA_INTERNAL'))
         ]
         self.extra_commands = {
-            'import': ImportCertificateAuthorityCommand()
+            'import': ImportCertificateAuthorityCommand(self)
         }
 
         self.localdoc['CreateEntityCommand'] = ("""\
@@ -86,6 +87,9 @@ class CertificateAuthorityNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoa
             create type=CA_INTERMEDIATE signedby=myCA name=myInterCA key_length=2048 digest_algorithm=SHA256
             lifetime=3650 country=PL state=Slaskie city=Czerwionka-Leszczyny organization=myorg email=a@b.c
             common=MyCommonName
+
+            import type=CA_EXISTING name=myimportedCA certificate=<certificate> privatekey=<privatekey>
+            passphrase=<passphrase> serial=<serial_number>
 
             Crates a Certificate Authority. For a list of properties, see 'help properties'.""")
 
@@ -113,24 +117,28 @@ class CertificateAuthorityNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoa
             name='certificate',
             get='certificate',
             set='certificate',
-            condition=lambda e: e['type'] == 'CA_EXISTING',
-            list=True)
+            list=False)
 
         self.add_property(
             descr='Private Key',
             name='privatekey',
             get='privatekey',
             set='privatekey',
-            condition=lambda e: e['type'] == 'CA_EXISTING',
-            list=True)
+            list=False)
 
         self.add_property(
             descr='Serial',
             name='serial',
             get='serial',
             set='serial',
-            condition=lambda e: e['type'] == 'CA_EXISTING',
-            list=True)
+            list=False)
+
+        self.add_property(
+            descr='Pass Phrase',
+            name='passphrase',
+            get='passphrase',
+            set='passphrase',
+            list=False)
 
         self.add_property(
             descr='Key length',
@@ -221,14 +229,40 @@ class CertificateAuthorityNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoa
 @description("Imports given CA")
 class ImportCertificateAuthorityCommand(Command):
     """
-    Usage: TODO
-
-    Example: TODO
+    Usage: import type=CA_EXISTING name=<name> certificate=<certificate> privatekey=<privatekey>
+           [passphrase=<passphrase>] serial=<serial_number>
 
     Imports a Certificate Authority.
     """
+    def __init__(self, parent):
+        self.parent = parent
+
     def run(self, context, args, kwargs, opargs):
-        pass
+        if not kwargs:
+            raise CommandException(_("Import requires more arguments, see 'help import' for more information"))
+        if 'type' not in kwargs or kwargs['type'] != "CA_EXISTING":
+            raise CommandException(_("Pleaes specify valid 'type' argument value"))
+        if 'name' not in kwargs:
+            raise CommandException(_("Please specify name of the imported CA"))
+        if 'certificate' not in kwargs:
+            raise CommandException(_("Please specify 'certificate' argument"))
+        if 'privatekey' not in kwargs:
+            raise CommandException(_("Please specify 'privatekey' argument"))
+        if 'serial' not in kwargs:
+            raise CommandException(_("Please specify 'serial' argument"))
+
+        context.submit_task(self.parent.import_task, kwargs)
+
+    def complete(self, context):
+        return [
+            NullComplete('name='),
+            NullComplete('certificate='),
+            NullComplete('privatekey='),
+            NullComplete('passphrase='),
+            NullComplete('serial='),
+            EnumComplete('type=', ['CA_EXISTING'])
+        ]
+
 
 @description(_("Provides access to Certificate actions"))
 class CertificateNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, EntityNamespace):
@@ -248,13 +282,17 @@ class CertificateNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             ('type', 'in', ('CERT_INTERNAL', 'CERT_CSR', 'CERT_INTERMEDIATE', 'CERT_EXISTING'))
         ]
         self.extra_commands = {
-            'import': ImportCertificateCommand()
+            'import': ImportCertificateCommand(self)
         }
 
         self.localdoc['CreateEntityCommand'] = ("""\
-            Examples: create type=CERT_INTERNAL name=myCert signedby=myCA key_length=2048
+            Examples:
+            create type=CERT_INTERNAL name=myCert signedby=myCA key_length=2048
             digest_algorithm=SHA256 lifetime=3650 country=PL state=Slaskie city=Czerwionka-Leszczyny
             organization=myorg email=a@b.c common=MyCommonName
+
+            import type=CERT_EXISTING name=myimportedCert certificate=<certificate> privatekey=<privatekey>
+            [passphrase=<passphrase>]
 
             Crates a certificate. For a list of properties, see 'help properties'.""")
 
@@ -283,16 +321,14 @@ class CertificateNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             name='certificate',
             get='certificate',
             set='certificate',
-            condition=lambda e: e['type'] == 'CERT_EXISTING',
-            list=True)
+            list=False)
 
         self.add_property(
             descr='Private Key',
             name='privatekey',
             get='privatekey',
             set='privatekey',
-            condition=lambda e: e['type'] == 'CERT_EXISTING',
-            list=True)
+            list=False)
 
         self.add_property(
             descr='Signing CA',
@@ -385,15 +421,37 @@ class CertificateNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
 @description("Imports given CA")
 class ImportCertificateCommand(Command):
     """
-    Usage: TODO
+    Usage: Usage: import type=CERT_EXISTING name=<name> certificate=<certificate> privatekey=<privatekey>
+           [passphrase=<passphrase>]
 
-    Example: TODO
-
-    Imports a Certificate Authority.
+    Imports a Certificate.
     """
+    def __init__(self, parent):
+        self.parent = parent
+
     def run(self, context, args, kwargs, opargs):
-        pass
+        if not kwargs:
+            raise CommandException(_("Import requires more arguments, see 'help import' for more information"))
+        if 'type' not in kwargs or kwargs['type'] != "CERT_EXISTING":
+            raise CommandException(_("Pleaes specify valid 'type' argument value"))
+        if 'name' not in kwargs:
+            raise CommandException(_("Please specify name of the imported Certificate"))
+        if 'certificate' not in kwargs:
+            raise CommandException(_("Please specify 'certificate' argument"))
+        if 'privatekey' not in kwargs:
+            raise CommandException(_("Please specify 'privatekey' argument"))
+
+        context.submit_task(self.parent.import_task, kwargs)
+
+    def complete(self, context):
+        return [
+            NullComplete('name='),
+            NullComplete('certificate='),
+            NullComplete('privatekey='),
+            NullComplete('passphrase='),
+            EnumComplete('type=', ['CERT_EXISTING'])
+        ]
+
 
 def _init(context):
-    #context.attach_namespace('/', CryptoNamespace('crypto', context))
-    pass
+    context.attach_namespace('/', CryptoNamespace('crypto', context))

1802fcf515ab9a6e61b9f85f1980df0f740ca171
Wojciech Kloska <shangteus@ixsystems.com>
2016-09-28 21:37:10 +0200

    UPDATED: simplify replication link create process and add some completers

--- freenas/cli/plugins/replication.py
+++ freenas/cli/plugins/replication.py
@@ -25,16 +25,14 @@
 #
 #####################################################################
 
-import copy
-import six
 import gettext
 from freenas.cli.namespace import (
-    SingleItemNamespace, Command, CommandException, EntityNamespace, TaskBasedSaveMixin,
+    Command, CommandException, EntityNamespace, TaskBasedSaveMixin,
     EntitySubscriberBasedLoadMixin, description
 )
-from freenas.cli.complete import NullComplete, EnumComplete
-from freenas.cli.output import ValueType, read_value
-from freenas.cli.utils import TaskPromise, EntityPromise,post_save, parse_timedelta
+from freenas.cli.complete import EntitySubscriberComplete, MultipleSourceComplete, RpcComplete
+from freenas.cli.output import ValueType
+from freenas.cli.utils import TaskPromise, post_save, parse_timedelta
 from freenas.utils import query as q
 
 
@@ -99,194 +97,6 @@ class SwitchCommand(Command):
         return TaskPromise(context, tid)
 
 
-@description(_("Creates a replication link"))
-class CreateReplicationCommand(Command):
-    """
-    Usage: create <name> master=<master> slave=<slave> recursive=<recursive>
-            bidirectional=<bidirectional> auto_recover=<auto_recover>
-            replicate_services=<replicate_services> encrypt=<encrypt>
-            compress=<fast/default/best> throttle=<throttle>
-            snapshot_lifetime=<snapshot_lifetime> follow_delete=<follow_delete>
-
-    Example: create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool,mypool/dataset
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool recursive=yes
-             create my_replication master=10.0.0.2 slave=10.0.0.3 datasets=mypool
-                                   bidirectional=yes
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool,mypool2 bidirectional=yes
-                                   recursive=yes
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool,mypool2 bidirectional=yes
-                                   recursive=yes replicate_services=yes
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool,mypool2 bidirectional=yes
-                                   recursive=yes replicate_services=yes
-                                   auto_recover=yes
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool encrypt=AES128
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool compress=best
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool throttle=10MiB
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool encrypt=AES128 compress=best
-                                   throttle=10MiB
-             create my_replication master=10.0.0.2 slave=10.0.0.3
-                                   datasets=mypool snapshot_lifetime=1:10:10
-                                   followdelete=yes
-
-    Creates a replication link entry. Link contains configuration data
-    used in later replication process.
-
-    All ZFS pools referenced in 'datasets' property must exist on both
-    slave and master at creation time. They also need to have the same names.
-
-    Created replication is implicitly: unidirectional, non-recursive,
-    does not recover automatically and does not replicate services
-    along with datasets.
-
-    One of: master, slave parameters must represent one of current machine's
-    IP addresses. Both these parameters must be defined,
-    because unidirectional replication link can be promoted
-    to become bi-directional link.
-
-    Recursive parameter set to 'yes' informs that every child dataset
-    of datasets defined in 'datasets' parameter will be replicated
-    along with provided parents.
-
-    Only in bi-directional replication service replication
-    and automatic recovery are available.
-
-    When automatic recovery is selected it is not possible to switch
-    hosts roles manually. It's being done automatically each time
-    'master' goes down or up again.
-    """
-
-    def __init__(self, parent):
-        self.parent = parent
-
-    def run(self, context, args, kwargs, opargs):
-        if not args and not kwargs:
-            raise CommandException(_("Create requires more arguments, see 'help create' for more information"))
-        if len(args) > 1:
-            raise CommandException(_("Wrong syntax for create, see 'help create' for more information"))
-
-        if len(args) == 1:
-            kwargs[self.parent.primary_key.name] = args.pop(0)
-
-        if 'name' not in kwargs:
-            raise CommandException(_('Please specify a name for your replication link'))
-        else:
-            name = kwargs.pop('name')
-
-        master = kwargs.pop('master')
-        slave = kwargs.pop('slave')
-        partners = [master, slave]
-
-        for ip in context.call_sync('network.config.get_my_ips'):
-            if ip in partners:
-                break
-        else:
-            raise CommandException(_(
-                'None of provided replication link partners {0}, {1} match any of machine\'s IPs'.format(master, slave)
-            ))
-
-        datasets = kwargs.pop('datasets', [])
-        if isinstance(datasets, six.string_types):
-            datasets = [datasets]
-        bidirectional = read_value(kwargs.pop('bidirectional', False), ValueType.BOOLEAN)
-        auto_recover = read_value(kwargs.pop('auto_recover', False), ValueType.BOOLEAN)
-        recursive = read_value(kwargs.pop('recursive', False), ValueType.BOOLEAN)
-        replicate_services = read_value(kwargs.pop('replicate_services', False), ValueType.BOOLEAN)
-
-        if not bidirectional:
-            if replicate_services:
-                raise CommandException(_(
-                    'Replication of services is available only when bi-directional replication is selected'
-                ))
-
-            if auto_recover:
-                raise CommandException(_(
-                    'Automatic recovery is available only when bi-directional replication is selected'
-                ))
-
-        ns = SingleItemNamespace(None, self.parent, context)
-        ns.orig_entity = copy.deepcopy(self.parent.skeleton_entity)
-        ns.entity = copy.deepcopy(self.parent.skeleton_entity)
-
-        ns.entity['name'] = name
-        ns.entity['master'] = master
-        ns.entity['partners'] = partners
-        ns.entity['datasets'] = datasets
-        ns.entity['bidirectional'] = bidirectional
-        ns.entity['auto_recover'] = auto_recover
-        ns.entity['recursive'] = recursive
-        ns.entity['replicate_services'] = replicate_services
-
-        compress = kwargs.pop('compress', None)
-        encrypt = kwargs.pop('encrypt', 'AES128')
-        throttle = kwargs.pop('throttle', None)
-        transport_plugins = []
-
-        if compress:
-            if compress not in ['no', 'fast', 'default', 'best']:
-                raise CommandException('Compression level must be selected as one of: fast, default, best')
-            if compress != ' no':
-                transport_plugins.append({
-                    'name': 'compress',
-                    'level': compress.upper()
-                })
-
-        if throttle:
-            if not isinstance(throttle, int):
-                raise CommandException('Throttle must be a number representing maximum transfer per second')
-            transport_plugins.append({
-                'name': 'throttle',
-                'buffer_size': throttle
-            })
-
-        if encrypt:
-            if encrypt not in ['no', 'AES128', 'AES192', 'AES256']:
-                raise CommandException('Encryption type must be selected as one of: no, AES128, AES192, AES256')
-            if encrypt != 'no':
-                transport_plugins.append({
-                    'name': 'encrypt',
-                    'type': encrypt.upper()
-                })
-
-        ns.entity['transport_options'] = transport_plugins
-
-        ns.entity['snapshot_lifetime'] = parse_timedelta(kwargs.get('snapshot_lifetime', 365 * 24 * 60 * 60)).seconds
-        ns.entity['followdelete'] = kwargs.get('followdelete', False)
-
-        tid = context.submit_task(
-            self.parent.create_task,
-            ns.entity,
-            callback=lambda s, t: post_save(ns, s, t)
-        )
-
-        return EntityPromise(context, tid, ns)
-
-    def complete(self, context, **kwargs):
-        return [
-            NullComplete('name='),
-            NullComplete('master='),
-            NullComplete('slave='),
-            NullComplete('datasets='),
-            NullComplete('throttle='),
-            NullComplete('snapshot_lifetime='),
-            EnumComplete('recursive=', ['yes', 'no']),
-            EnumComplete('followdelete=', ['yes', 'no']),
-            EnumComplete('bidirectional=', ['yes', 'no']),
-            EnumComplete('auto_recover=', ['yes', 'no']),
-            EnumComplete('replicate_services=', ['yes', 'no']),
-            EnumComplete('compress=', ['no', 'fast', 'default', 'best']),
-            EnumComplete('encrypt=', ['no', 'AES128', 'AES192', 'AES256'])
-        ]
-
-
 @description(_("List and manage replication tasks"))
 class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, EntityNamespace):
     """
@@ -295,13 +105,85 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
     def __init__(self, name, context):
         super(ReplicationNamespace, self).__init__(name, context)
 
+        class PeerComplete(MultipleSourceComplete):
+            def __init__(self, name):
+                super(PeerComplete, self).__init__(
+                    name,
+                    (
+                        EntitySubscriberComplete(name, 'peer', lambda o: o['name'] if o['type'] == 'freenas' else None),
+                        RpcComplete(name, 'system.general.get_config', lambda o: o['hostname'])
+                    )
+                )
+
         self.primary_key_name = 'name'
-        self.save_key_name = 'name'
+        self.save_key_name = 'id'
         self.entity_subscriber_name = 'replication'
         self.create_task = 'replication.create'
         self.update_task = 'replication.update'
         self.delete_task = 'replication.delete'
 
+        self.localdoc['CreateEntityCommand'] = ("""\
+            Usage: create <name> master=<master> slave=<slave> recursive=<recursive>
+                    bidirectional=<bidirectional> auto_recover=<auto_recover>
+                    replicate_services=<replicate_services> encrypt=<encrypt>
+                    compress=<fast/default/best> throttle=<throttle>
+                    snapshot_lifetime=<snapshot_lifetime> follow_delete=<follow_delete>
+
+            Example: create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool,mypool/dataset
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool recursive=yes
+                     create my_replication master=10.0.0.2 slave=10.0.0.3 datasets=mypool
+                                           bidirectional=yes
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool,mypool2 bidirectional=yes
+                                           recursive=yes
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool,mypool2 bidirectional=yes
+                                           recursive=yes replicate_services=yes
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool,mypool2 bidirectional=yes
+                                           recursive=yes replicate_services=yes
+                                           auto_recover=yes
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool encrypt=AES128
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool compress=best
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool throttle=10MiB
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool encrypt=AES128 compress=best
+                                           throttle=10MiB
+                     create my_replication master=10.0.0.2 slave=10.0.0.3
+                                           datasets=mypool snapshot_lifetime=1:10:10
+                                           followdelete=yes
+
+            Creates a replication link entry. Link contains configuration data
+            used in later replication process.
+
+            All ZFS pools referenced in 'datasets' property must exist on both
+            slave and master at creation time. They also need to have the same names.
+
+            Created replication is implicitly: unidirectional, non-recursive,
+            does not recover automatically and does not replicate services
+            along with datasets.
+
+            One of: master, slave parameters must represent one of current machine's
+            IP addresses. Both these parameters must be defined,
+            because unidirectional replication link can be promoted
+            to become bi-directional link.
+
+            Recursive parameter set to 'yes' informs that every child dataset
+            of datasets defined in 'datasets' parameter will be replicated
+            along with provided parents.
+
+            Only in bi-directional replication service replication
+            and automatic recovery are available.
+
+            When automatic recovery is selected it is not possible to switch
+            hosts roles manually. It's being done automatically each time
+            'master' goes down or up again.
+            Creates a replication task. For a list of properties, see 'help properties'.""")
         self.entity_localdoc['SetEntityCommand'] = ("""\
             Usage: set <property>=<value> ...
 
@@ -412,6 +294,7 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             get='master',
             set='master',
             list=False,
+            complete=PeerComplete('master='),
             usage=_('Name of FreeNAS machine (peer) acting as a sending side.')
         )
 
@@ -420,8 +303,8 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             name='slave',
             get='slave',
             usersetable=False,
-            type=ValueType.SET,
             list=True,
+            complete=PeerComplete('slave='),
             usage=_('Name of FreeNAS machine (peer) acting as a receiving side.')
         )
 
@@ -432,6 +315,7 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             set='datasets',
             list=False,
             type=ValueType.SET,
+            complete=EntitySubscriberComplete('datasets=', 'volume.dataset', lambda o: o['name']),
             usage=_('List of datasets to be replicated.')
         )
 
@@ -465,6 +349,7 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             name='initial_master',
             get='initial_master',
             usersetable=False,
+            createsetable=False,
             list=False,
             usage=_('Informs which host was initially selected a replication master.')
         )
@@ -544,6 +429,7 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             set='followdelete',
             list=False,
             type=ValueType.BOOLEAN,
+            enum=['yes', 'no'],
             usage=_('''\
             Defines if replication should automatically remove
             stale snapshots at slave side.''')
@@ -554,6 +440,7 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             name='result',
             get='status.status',
             usersetable=False,
+            createsetable=False,
             list=False,
             type=ValueType.STRING,
             usage=_('String status of last replication run.')
@@ -564,6 +451,7 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             name='message',
             get='status.message',
             usersetable=False,
+            createsetable=False,
             list=False,
             type=ValueType.STRING,
             usage=_('Output message of last replication run.')
@@ -574,6 +462,7 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             name='size',
             get='status.size',
             usersetable=False,
+            createsetable=False,
             list=False,
             type=ValueType.SIZE,
             usage=_('Transfer size of last replication run.')
@@ -584,6 +473,7 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
             name='speed',
             get='status.speed',
             usersetable=False,
+            createsetable=False,
             list=False,
             type=ValueType.SIZE,
             usage=_('Transfer speed of last replication run.')
@@ -592,11 +482,6 @@ class ReplicationNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, E
         self.primary_key = self.get_mapping('name')
 
         self.entity_commands = self.get_entity_commands
-
-    def commands(self):
-        cmds = super(ReplicationNamespace, self).commands()
-        cmds.update({'create': CreateReplicationCommand(self)})
-        return cmds
 
     def get_entity_commands(self, this):
         this.load()

5ae844fab9477968cf49e805cf09daf51879cbb3
Jakub Klama <jakub@ixsystems.com>
2016-10-06 10:28:33 +0200

    Temporary CLI build fix

--- freenas/cli/plugins/volumes.py
+++ freenas/cli/plugins/volumes.py
@@ -958,12 +958,13 @@ class DatasetsNamespace(EntitySubscriberBasedLoadMixin, TaskBasedSaveMixin, Enti
                 show | search name ~= mypool
                 show | search compression == lz4 | sort name""")
 
-        self.skeleton_entity = {
-            'type': 'FILESYSTEM',
-            'volume': self.parent.entity['id'],
-            'temp_mountpoint': None,
-            'properties': {}
-        }
+        if not getattr(self, 'is_docgen_instance', False):
+            self.skeleton_entity = {
+                'type': 'FILESYSTEM',
+                'volume': self.parent.entity['id'],
+                'temp_mountpoint': None,
+                'properties': {}
+            }
 
         self.add_property(
             descr='Name',

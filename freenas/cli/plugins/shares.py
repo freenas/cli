45b11b1b80e647366c8b5629587c9eab4dea5de3
Jakub Klama <jakub@ixsystems.com>
2016-10-07 16:28:46 +0200

    Unify the discriminator value

--- freenas/cli/plugins/shares.py
+++ freenas/cli/plugins/shares.py
@@ -217,7 +217,7 @@ class BaseSharesNamespace(TaskBasedSaveMixin, EntitySubscriberBasedLoadMixin, En
             'enabled': True,
             'target_type': 'DATASET',
             'properties': {
-                'type': 'share-{0}'.format(type_name)
+                '%type': 'share-{0}'.format(type_name)
             }
         }
 

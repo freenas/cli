8b5f01330874044d49aad3ad8c92a57451c41d39
Jakub Klama <jakub@ixsystems.com>
2016-10-08 23:02:25 +0200

    Experimentally try to cache yacc parser.

--- freenas/cli/parser.py
+++ freenas/cli/parser.py
@@ -1077,7 +1077,7 @@ def p_error(p):
 
 
 lexer = lex.lex()
-parser = yacc.yacc(debug=False, optimize=True, write_tables=False)
+parser = yacc.yacc(debug=False, optimize=True)
 
 
 def parse(s, filename, recover_errors=False):

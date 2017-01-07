b73ec99b878b0e482c95d8dade1e0f2417f25ab4
650elx <bprokop02@gmail.com>
2017-01-07 03:47:13 +0100

    Create CLI command shouldn't complete delete args

--- freenas/cli/namespace.py
+++ freenas/cli/namespace.py
@@ -1189,7 +1189,11 @@ class CreateEntityCommand(Command):
                 with contextlib.suppress(BaseException):
                     prop.do_set(ns.entity, v)
 
-            return [create_completer(x) for x in self.parent.property_mappings if x.can_set(ns.entity) and x.createsetable]
+            return [
+                create_completer(x)
+                for x in self.parent.property_mappings
+                if x.can_set(ns.entity) and x.createsetable and not x.delete_arg
+            ]
 
         return []
 

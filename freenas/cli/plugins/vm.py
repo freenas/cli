b3903ecc76092480c08105e4abaf1c6e38df37d5
Jakub Klama <jakub@ixsystems.com>
2017-02-01 01:00:46 +0100

    Add CLI support for SCSI HBA devices. While here, fix remaining schema names.

--- freenas/cli/plugins/vm.py
+++ freenas/cli/plugins/vm.py
@@ -755,6 +755,38 @@ class VMDeviceGraphicsPropertiesMixin(BaseVariantMixin):
         return "VGA device with resolution {0}".format(get(o, 'properties.resolution'))
 
 
+class VMDeviceScsiPropertiesMixin(BaseVariantMixin):
+    """
+    The VM Device Usb namespace provides commands for managing USB resources
+    available on selected virtual machine
+    """
+    def add_properties(self):
+        super(VMDeviceScsiPropertiesMixin, self).add_properties()
+
+        self.add_property(
+            descr='Port number',
+            name='port',
+            get='properties.port',
+            list=False,
+            type=ValueType.NUMBER,
+            condition=lambda o: o['type'] == 'SCSI',
+        )
+
+        self.add_property(
+            descr='Initiator ID',
+            name='initiator_id',
+            get='properties.initiator_id',
+            list=False,
+            type=ValueType.NUMBER,
+            condition=lambda o: o['type'] == 'SCSI',
+        )
+
+    @staticmethod
+    def get_humanized_summary(o):
+        return "SCSI host bus adapter device"
+
+
+
 class VMDeviceUsbPropertiesMixin(BaseVariantMixin):
     """
     The VM Device Usb namespace provides commands for managing USB resources
@@ -931,7 +963,7 @@ class VMDeviceCdromPropertiesMixin(BaseVariantMixin):
 
 class VMDeviceNamespace(
     NestedObjectLoadMixin, NestedObjectSaveMixin, EntityNamespace,
-    VMDeviceGraphicsPropertiesMixin, VMDeviceDiskPropertiesMixin,
+    VMDeviceGraphicsPropertiesMixin, VMDeviceDiskPropertiesMixin, VMDeviceScsiPropertiesMixin,
     VMDeviceUsbPropertiesMixin, VMDeviceNicPropertiesMixin, VMDeviceCdromPropertiesMixin,
     BaseVariantMixin
 ):
@@ -946,7 +978,7 @@ class VMDeviceNamespace(
         self.primary_key_name = 'name'
         self.parent = parent
         self.parent_path = 'devices'
-        self.extra_query_params = [('type', 'in', ('GRAPHICS', 'CDROM', 'NIC', 'USB', 'DISK'))]
+        self.extra_query_params = [('type', 'in', ('GRAPHICS', 'CDROM', 'NIC', 'USB', 'DISK', 'SCSI'))]
         self.required_props = ['name']
         self.skeleton_entity = {
             'name': None,
@@ -959,6 +991,7 @@ class VMDeviceNamespace(
             'CDROM': VMDeviceCdromPropertiesMixin.get_humanized_summary,
             'NIC': VMDeviceNicPropertiesMixin.get_humanized_summary,
             'USB': VMDeviceUsbPropertiesMixin.get_humanized_summary,
+            'SCSI': VMDeviceScsiPropertiesMixin.get_humanized_summary,
             'GRAPHICS': VMDeviceGraphicsPropertiesMixin.get_humanized_summary
         }
 
@@ -1004,7 +1037,7 @@ class VMDeviceNamespace(
             get='type',
             set='type',
             list=True,
-            enum=['GRAPHICS', 'NIC', 'DISK', 'CDROM', 'USB']
+            enum=['GRAPHICS', 'NIC', 'DISK', 'CDROM', 'USB', 'SCSI']
         )
 
         self.add_property(
@@ -1020,11 +1053,12 @@ class VMDeviceNamespace(
 
     def save(self, this, new=False):
         types = {
-            'DISK': 'vm-device-disk',
-            'CDROM': 'vm-device-cdrom',
-            'NIC': 'vm-device-nic',
-            'USB': 'vm-device-usb',
-            'GRAPHICS': 'vm-device-graphics'
+            'DISK': 'VmDeviceDisk',
+            'CDROM': 'VmDeviceCdrom',
+            'NIC': 'VmDeviceNic',
+            'USB': 'VmDeviceUsb',
+            'GRAPHICS': 'VmDeviceGraphics',
+            'SCSI': 'VmDeviceScsi'
         }
 
         if new:

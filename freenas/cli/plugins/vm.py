4ec1391f13fe15eb668107f9b55fe94f6d1a3d7d
Piotr Glinianowicz <piotr@ixsystems.com>
2017-02-02 22:37:51 +0100

    Revert "UPDATED: renamed ZVOL to BLOCK"
    This reverts commit 8cf4ac576f21aa1caf911db12249ce235b3e5767.

--- freenas/cli/plugins/vm.py
+++ freenas/cli/plugins/vm.py
@@ -872,7 +872,7 @@ class VMDeviceDiskPropertiesMixin(BaseVariantMixin):
             name='target_type',
             get='properties.target_type',
             list=False,
-            enum=['BLOCK', 'FILE', 'DISK'],
+            enum=['ZVOL', 'FILE', 'DISK'],
             condition=lambda o: o['type'] == 'DISK',
         )
 
